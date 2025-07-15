1fd18d8f2078bd115a5b4b2f8332a1fc4cdae240
satvikapadakanti <satvikapadakanti@gmail.com>
2025-07-15 18:51:54 +0530

    added solution for removing node from linked list

---
+++ remove.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode slow=dummy;
+        ListNode fast=dummy;
+        int count=0;
+        while(count<=n){
+            count++;
+            fast=fast.next;
+        }
+        while(fast!=null){
+            slow=slow.next;
+            fast=fast.next;
+        }
+        slow.next=slow.next.next;
+        return dummy.next;
+
+    }
+}
